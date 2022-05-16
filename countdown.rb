849f51a252a54ef759c78ee269578bff2990249b
CatherineHonors <cathonors@gmail.com>
2022-05-16 10:00:52 -0400

    commit tree 5fb52f5e99a2aee9091db5b8641c301aff2914ad

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,19 @@
 #write your code here
 
-def countdown
+
+
+def countdown_with_sleep num
+    sleep 1
 end
+
+
+def countdown (x, countdown_with_sleep)
+    
+    while x >= 1
+        puts "#{x} SECOND(S)!"
+        x -= 1
+    end
+    return "HAPPY NEW YEAR!"
+end
+
+countdown(23, countdown_with_sleep)
